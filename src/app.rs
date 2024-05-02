2ea7aac18dcef51783dc3d2c81702bbe3c311e51
Markus Krause <Krause.Markus@gmx.com>
2024-05-02 13:41:05 +0200

    intermediate state for debugging on Linux

--- src/app.rs
+++ src/app.rs
@@ -1,7 +1,9 @@
 use std::{
     collections::VecDeque,
     fs::{create_dir_all, read_to_string},
+    io::{Read, Write},
     path::Path,
+    time::Duration,
 };
 
 use arboard::Clipboard;
@@ -11,11 +13,17 @@ use egui_dropdown::DropDownBox;
 use egui_extras::{Column, TableBuilder};
 use glob::glob;
 use indexmap::{indexmap, IndexMap};
+use mio::{Events, Interest, Poll, Token};
+use mio_serial::{DataBits, SerialPort, SerialPortInfo};
+use mio_serial::{SerialPortBuilderExt, SerialStream};
 use regex::Regex;
+use std::io;
 use subprocess::Exec;
 use tempdir::TempDir;
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
+
+const SERIAL_TOKEN: Token = Token(0);
 
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 #[derive(serde::Deserialize, serde::Serialize)]
@@ -27,7 +35,17 @@ pub struct MyApp {
     stop_bits: u32,
     parity: bool,
     connect_on_startup: bool,
-    history: VecDeque<String>,
+    portlist: VecDeque<String>,
+    #[serde(skip)]
+    poll: Poll,
+    #[serde(skip)]
+    events: Events,
+    #[serde(skip)]
+    serial: Option<SerialStream>,
+    #[serde(skip)]
+    device: String,
+    #[serde(skip)]
+    ports: Vec<SerialPortInfo>,
     #[serde(skip)]
     tempdir: Option<TempDir>,
     #[serde(skip)]
@@ -45,7 +63,12 @@ impl Default for MyApp {
             stop_bits: 1,
             parity: false,
             connect_on_startup: false,
-            history: VecDeque::with_capacity(11),
+            portlist: VecDeque::with_capacity(11),
+            poll: Poll::new().unwrap(), // if this does not work there's no point in running anyway
+            events: Events::with_capacity(1),
+            serial: None,
+            device: "".to_owned(),
+            ports: vec![],
             tempdir: TempDir::new("rustymeter").ok(),
             settings_open: false,
             is_init: false,
@@ -83,7 +106,50 @@ impl eframe::App for MyApp {
 
         // on startup handle certain items once
         if !self.is_init {
+            if let Ok(ports) = mio_serial::available_ports() {
+                for p in ports {
+                    self.portlist.push_front(p.port_name);
+                }
+            }
             self.is_init = true
+        }
+
+        if let Some(serial) = &mut self.serial {
+            // Poll to check if we have serial events waiting for us.
+            let res = self.poll.poll(&mut self.events, None);
+            println!("Poll: {:?}", res);
+
+            // buffer for read data
+            let mut buf = vec![];
+
+            // Process each event.
+            for event in self.events.iter() {
+                // Validate the token we registered our socket with,
+                // in this example it will only ever be one but we
+                // make sure it's valid none the less.
+                match event.token() {
+                    SERIAL_TOKEN => loop {
+                        // In this loop we receive all packets queued for the socket.
+                        match serial.read(&mut buf) {
+                            Ok(count) => {
+                                println!("{:?}", String::from_utf8_lossy(&buf[..count]));
+                                self.device = String::from_utf8_lossy(&buf).to_string();
+                            }
+                            Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
+                                println!("WouldBlock escape");
+                                break;
+                            }
+                            Err(e) => {
+                                println!("Quitting due to read error: {}", e);
+                                // return Err(e);
+                            }
+                        }
+                    },
+                    _ => {
+                        // This should never happen as there is only one port open
+                    }
+                }
+            }
         }
 
         egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
@@ -120,8 +186,8 @@ impl eframe::App for MyApp {
 
                     ui.add(
                         DropDownBox::from_iter(
-                            &self.history,
-                            "searchbox",
+                            &self.portlist,
+                            "portlistbox",
                             &mut self.serial_port,
                             |ui, text| ui.selectable_label(false, text),
                         )
@@ -130,9 +196,39 @@ impl eframe::App for MyApp {
                         .filter_by_input(false),
                     );
 
-                    if ui.button("Connect").clicked() {}
+                    if ui.button("Connect").clicked() {
+                        self.serial = mio_serial::new(&self.serial_port, self.baud_rate)
+                            .open_native_async()
+                            .ok();
+                        if let Some(serial) = &mut self.serial {
+                            let res = self.poll.registry().register(
+                                serial,
+                                SERIAL_TOKEN,
+                                Interest::READABLE | Interest::WRITABLE,
+                            );
+                            println!("Registry: {:?}", res);
+                            serial.set_data_bits(DataBits::Eight);
+                            serial.set_stop_bits(mio_serial::StopBits::One);
+                            serial.set_parity(mio_serial::Parity::None);
+                            let res = serial.write("SYST:REM\n".as_bytes());
+                            // println!("{:?}", res);
+                            // let res = serial.flush();
+                            // println!("{:?}", res);
+                            //let res = serial.write("*IDN?\n".as_bytes());
+                            // println!("*IDN?: {:?}", res);
+                            let res = serial.write("MEAS?\n".as_bytes());
+                            println!("MEAS?: {:?}", res);
+                        }
+                    }
                 });
-                ui.horizontal(|ui| {});
+                ui.horizontal(|ui| {
+                    if !self.device.is_empty() {
+                        ui.label("Connected to: ");
+                        ui.label(&self.device);
+                    } else {
+                        ui.label("Not connected.");
+                    }
+                });
             });
 
             ui.separator();
@@ -163,18 +259,21 @@ impl eframe::App for MyApp {
                         ui.vertical(|ui| {
                             ui.heading("Settings");
                             ui.checkbox(&mut self.connect_on_startup, "Connect on startup");
-                            ui.checkbox(&mut self.parity, "Use parity bit");
+                            ui.checkbox(
+                                &mut self.parity,
+                                "Use parity bit (ignored right now, always None)",
+                            );
                             ui.label("Baud rate:");
                             ui.add(
                                 TextEdit::singleline(&mut self.baud_rate.to_string())
                                     .desired_width(800.0),
                             );
-                            ui.label("Bits:");
+                            ui.label("Data Bits (ignored right now, always 8):");
                             ui.add(
                                 TextEdit::singleline(&mut self.bits.to_string())
                                     .desired_width(800.0),
                             );
-                            ui.label("Stop bits:");
+                            ui.label("Stop bits (ignored right now, always 1):");
                             ui.add(
                                 TextEdit::singleline(&mut self.stop_bits.to_string())
                                     .desired_width(800.0),
